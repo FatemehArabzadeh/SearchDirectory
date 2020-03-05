bcf601b217418cbad0b8f540de567a75caa45fdb
fatemeh arabzadeh <f.arabzadeh@ymail.com>
2020-03-05 19:25:44 +0330

    Complete the documentation .

--- src/SearchInDirectory.java
+++ src/SearchInDirectory.java
@@ -11,6 +11,12 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
+/**
+ * This Class Include main method and it prints name of files in specific directory
+ * that matches with the arithmetic expression that user type in console.
+ *
+ * @author Fatemeh Arabzadeh
+ */
 public class SearchInDirectory {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
@@ -18,29 +24,35 @@ public class SearchInDirectory {
         File f = new File("d:\\ddd");
         List<String> AllFilesPath = new ArrayList<>();
         Boolean b;
+        int CheckIfNoTextFileMatch = 0;
         FilenameFilter textFilter = (dir, name) -> name.toLowerCase().endsWith(".txt");
         File[] files = f.listFiles(textFilter);
         for (File file : files) {
             AllFilesPath.add(file.getPath());
         }
+        if (AllFilesPath.size() == 0) {
+            System.out.println("There is no text File in this directory");
+            return;
+        }
         for (int i = 0; i < AllFilesPath.size(); i++) {
-            if (test.contains(" AND ") && !(test.contains(" OR ")))
-                b = searchWordByAND(AllFilesPath.get(i), test);
-            else if (test.contains(" OR ") && !(test.contains(" AND ")))
-                b = searchWordByOR(AllFilesPath.get(i), test);
-            else if (test.contains(" OR ") && test.contains(" AND "))
-                b = searchWordArithmetic(AllFilesPath.get(i), test);
-            else
-                b = searchWord(AllFilesPath.get(i), test);
-
+            b = searchWordArithmetic(AllFilesPath.get(i), test);
             if (b.equals(true)) {
                 System.out.println(new File(AllFilesPath.get(i)).getName());
+                CheckIfNoTextFileMatch = 1;
             }
         }
-
+        if (CheckIfNoTextFileMatch == 0)
+            System.out.println("no text file find in this directory that match this expression");
     }
-
-    ///////////////////////////////////////first step
+///////////////////////////////////////////////////////////////////////////////////
+    /**
+     * This method is for first step and just search a word in a text file.
+     *
+     * @param path The path of text file
+     * @param test The string that user type in console
+     * @throws IOException
+     * @return true if this file contains test string , false otherwise
+     */
     public static boolean searchWord(String path, String test) throws IOException {
         try (BufferedReader in = new BufferedReader(new FileReader(path))) {
             String line;
@@ -60,7 +72,15 @@ public class SearchInDirectory {
         }
     }
 
-    //////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////
+    /**
+     * This method is for Second step and search an arithmetic expression that just include AND in a text file.
+     *
+     * @param path The path of text file
+     * @param test The string that user type in console
+     * @throws IOException
+     * @return true if this file matches with the arithmetic expression , false otherwise
+     */
     public static boolean searchWordByAND(String path, String test) throws IOException {
         List<String> SearchList = new ArrayList<>();
         String[] SearchArray = test.split(" AND ");
@@ -87,7 +107,15 @@ public class SearchInDirectory {
         }
     }
 
-    /////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////
+    /**
+     * This method is for third step and search an arithmetic expression that just include OR in a text file.
+     *
+     * @param path The path of text file
+     * @param test The string that user type in console
+     * @throws IOException
+     * @return true if this file matches with the arithmetic expression, false otherwise
+     */
     public static boolean searchWordByOR(String path, String test) throws IOException {
         List<String> SearchList;
         SearchList = Arrays.asList(test.split(" OR "));
@@ -109,7 +137,15 @@ public class SearchInDirectory {
         }
     }
 
-    ////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////
+    /**
+     * This method is for forth step and search an arithmetic expression that include AND & OR in a text file.
+     *
+     * @param path The path of text file
+     * @param test The string that user type in console
+     * @throws IOException
+     * @return true if this file matches with the arithmetic expression , false otherwise
+     */
     public static boolean searchWordArithmetic(String path, String test) throws IOException {
         List<String> SearchList;
         SearchList = Arrays.asList(test.split(" OR "));
@@ -130,7 +166,14 @@ public class SearchInDirectory {
         }
     }
 
-    ////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////
+    /**
+     * This method check a string that include another string or not in a non case sensitive way .
+     *
+     * @param str The string that you want to search in it
+     * @param searchStr The string that you want to search it in str
+     * @return true if this string contains , false otherwise
+     */
     public static boolean containsIgnoreCase(String str, String searchStr) {
         if (str == null || searchStr == null)
             return false;
